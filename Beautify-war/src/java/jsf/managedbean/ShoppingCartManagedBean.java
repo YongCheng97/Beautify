97f6f3b57aa5793f28742a3fc473e49370b6b4ea
wzynus <e0191599@u.nus.edu>
2020-04-27 16:38:42 +0800

    added item enitity for shopping cart, updated product details page

--- Beautify-war/src/java/jsf/managedbean/ShoppingCartManagedBean.java
+++ Beautify-war/src/java/jsf/managedbean/ShoppingCartManagedBean.java
@@ -1,31 +1,89 @@
 package jsf.managedbean;
 
+import entity.Item;
 import entity.Product;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
-
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 @Named(value = "shoppingCartManagedBean")
 @SessionScoped
 public class ShoppingCartManagedBean implements Serializable {
 
-    private List<Product> productList;
-    
+    private List<Item> items;
+    private int amountToCart;
+
     public ShoppingCartManagedBean() {
-    }
-    
-    
-    
-
-    public List<Product> getProductList() {
-        return productList;
+        this.items = new ArrayList<Item>();
     }
 
-    public void setProductList(List<Product> productList) {
-        this.productList = productList;
+    public void addToCart(Product product) {
+        String msg = null;
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        int index = this.existsInCart(product);
+
+        if (this.amountToCart == 0) {
+            msg = "Choose at least 1";
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null));
+        } else if (this.amountToCart > product.getQuantityOnHand()) {
+            msg = "Available product in stock: " + product.getQuantityOnHand();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, null));
+        } else {
+            if (index == -1) {
+                this.items.add(new Item(product, this.amountToCart));
+                msg = "Added to the cart";
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+                
+            } else {
+                int quantity = this.items.get(index).getQuantity() + this.amountToCart;
+                this.items.get(index).setQuantity(quantity);
+                msg = "Updated the quantity";
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, msg, null));
+            }
+            this.amountToCart = 0;
+        }
     }
-    
-    
+
+public int existsInCart(Product product) {
+        for (int i = 0; i < this.items.size(); i++) {
+            if (this.items.get(i).getProduct().getProductId() == product.getProductId()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @return the items
+     */
+    public List<Item> getItems() {
+        return items;
+    }
+
+    /**
+     * @param items the items to set
+     */
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    /**
+     * @return the amountToCart
+     */
+    public int getAmountToCart() {
+        return amountToCart;
+    }
+
+    /**
+     * @param amountToCart the amountToCart to set
+     */
+    public void setAmountToCart(int amountToCart) {
+        this.amountToCart = amountToCart;
+    }
+
 }
