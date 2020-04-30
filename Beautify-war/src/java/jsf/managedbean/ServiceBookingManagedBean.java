527ff2c5e6de2754cb06ec7e0b5ae606bf921ba6
wzynus <e0191599@u.nus.edu>
2020-04-30 22:36:57 +0800

    updated service booking dialog, still got time problem

--- Beautify-war/src/java/jsf/managedbean/ServiceBookingManagedBean.java
+++ Beautify-war/src/java/jsf/managedbean/ServiceBookingManagedBean.java
@@ -3,16 +3,58 @@ package jsf.managedbean;
 
 import ejb.session.stateless.BookingSessionBeanLocal;
 import entity.Customer;
+import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.ejb.EJB;
+import javax.faces.event.ActionEvent;
 
 @Named(value = "serviceBookingManagedBean")
 @SessionScoped
 public class ServiceBookingManagedBean implements Serializable {
 
+    
+
+    @EJB(name = "BookingSessionBeanLocal")
+    private BookingSessionBeanLocal bookingSessionBeanLocal;
+
+    private Customer currentCustomer;
+    
+    private Date appointmentDate;
+    private String address;
+    private Date startTime;
+    private Date endTime;
+    
+    private SimpleDateFormat sdf;
+    /**
+     * Creates a new instance of ServiceBookingManagedBean
+     */
+    public ServiceBookingManagedBean() {
+        sdf = new SimpleDateFormat("HH:mm");
+    }
+    
+    public void test(ActionEvent event) throws IOException {
+        System.out.println("Start time: "+ this.appointmentDate );
+    }
+
+    /**
+     * @return the bookingDate
+     */
+    public Date getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    /**
+     * @param appointmentDate the bookingDate to set
+     */
+    public void setAppointmentDate(Date appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+    
     /**
      * @return the address
      */
@@ -27,31 +69,32 @@ public class ServiceBookingManagedBean implements Serializable {
         this.address = address;
     }
 
-    @EJB(name = "BookingSessionBeanLocal")
-    private BookingSessionBeanLocal bookingSessionBeanLocal;
-
-    private Customer currentCustomer;
-    
-    private Date bookingDate;
-    private String address;
     /**
-     * Creates a new instance of ServiceBookingManagedBean
+     * @return the startTime
      */
-    public ServiceBookingManagedBean() {
+    public Date getStartTime() {
+        return startTime;
     }
 
     /**
-     * @return the bookingDate
+     * @param startTime the startTime to set
      */
-    public Date getBookingDate() {
-        return bookingDate;
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
     }
 
     /**
-     * @param bookingDate the bookingDate to set
+     * @return the endTime
      */
-    public void setBookingDate(Date bookingDate) {
-        this.bookingDate = bookingDate;
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * @param endTime the endTime to set
+     */
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
     }
     
 }
