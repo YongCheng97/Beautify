607dad81970449600cc76235d057bb8ead034f62
wzynus <e0191599@u.nus.edu>
2020-05-04 15:06:07 +0800

    creating and delete of category done

--- Beautify-angular2/src/app/view-all-product-categories/view-all-product-categories.component.ts
+++ Beautify-angular2/src/app/view-all-product-categories/view-all-product-categories.component.ts
@@ -5,6 +5,7 @@ import { NgForm } from '@angular/forms';
 import { SessionService } from '../session.service';
 import { CategoryService } from '../category.service';
 import { Category } from '../category';
+import { CategoryTypeEnum } from '../category-type-enum.enum';
 
 @Component({
   selector: 'app-view-all-product-categories',
@@ -14,8 +15,25 @@ import { Category } from '../category';
 export class ViewAllProductCategoriesComponent implements OnInit {
 
 	productCategories: Category[];
-	
+	newProductCategory: Category;
+	productCategoryToDelete: Category;
 	cols: any[];
+	
+	rootCategories: Category[];
+	rootCategory: Category;
+	
+	newCategoryName: string;
+	newCategoryDesc: string;
+	newCategoryType: string;
+	newCategoryParentId: number;
+	
+	submitted: boolean;
+	resultSuccess: boolean;
+	resultError: boolean;
+	message: string;
+	
+	displayCreate: boolean = false;
+	displayDelete: boolean = false;
 
 	constructor(private router: Router,
 				private activatedRoute: ActivatedRoute,
@@ -23,7 +41,12 @@ export class ViewAllProductCategoriesComponent implements OnInit {
 				private categoryService: CategoryService,
 				) 
 				{ 
-				
+					this.newProductCategory = new Category();
+					this.submitted = false;
+					this.resultSuccess = false;
+					this.resultError = false;
+					this.newCategoryParentId = null;
+					this.newCategoryType = "PRODUCT";
 				}
 	
 	ngOnInit(): void {
@@ -32,9 +55,18 @@ export class ViewAllProductCategoriesComponent implements OnInit {
 			this.productCategories = response.categories;
 		},
 		error => {
-			console.log('********** ViewAllProductCategoriesComponentComponent.ts: ' + error);
+			console.log('********** ViewAllProductCategoriesComponent.ts: ' + error);
 		}
-		);
+		)
+		
+		this.categoryService.getRootCategories().subscribe(
+			response => {
+				this.rootCategories = response.categories;
+			},
+			error => {
+				console.log('********** ViewAllProductCategoriesComponent.ts: ' + error);
+			}
+		)
 		
 		this.cols = [
 		{ field: 'categoryId', header:'Category Id'},
@@ -43,6 +75,75 @@ export class ViewAllProductCategoriesComponent implements OnInit {
 		{ field: 'type', header:'Category type'}
 		];
 		
+		
+	}
+	clear() {
+		this.submitted = false;
+		this.newProductCategory = new Category();
+	}
+	
+	showCreateDialog() {
+		this.displayCreate = true;
+	}
+	
+	showDeleteDialog(productCategoryToDelete: Category) {
+		this.displayDelete = true;
+		this.productCategoryToDelete = productCategoryToDelete;
+	}
+	
+	create(createCategoryForm : NgForm) {
+		this.submitted = true;
+		this.newProductCategory.name = this.newCategoryName;
+		this.newProductCategory.description = this.newCategoryDesc;
+		this.newProductCategory.type = null;
+		this.newCategoryParentId = this.rootCategory.categoryId;
+		
+		if(createCategoryForm.valid) {
+			this.categoryService.createCategory(this.newProductCategory, this.newCategoryType, this.newCategoryParentId).subscribe(
+				response => {
+					let newProductCategoryId = response.categoryId;
+					this.resultSuccess = true;
+					this.resultError = false;
+					this.message = "New service category " + newProductCategoryId + " created successfully";
+					this.categoryService.getProductCategories().subscribe(
+						response => {
+							this.productCategories = response.categories;
+						},
+						error => {
+							console.log('********** ViewAllProductCategoriesComponent.ts: ' + error);
+						}
+					)
+				},
+				error => {
+					this.resultSuccess = false;
+					this.resultError = true;
+					this.message = "An error has occured while created the new category";
+					console.log('********** ViewAllProductCategoriesComponent.ts: ' + error);
+				}
+			)
+		}
+		this.displayCreate = false;
+	}
+	
+	deleteCategory(deleteCategoryForm: NgForm) {
+		this.categoryService.deleteCategory(this.productCategoryToDelete.categoryId).subscribe(
+			response => {
+				this.router.navigate(["/view-all-product-categories"]);
+		
+				this.categoryService.getProductCategories().subscribe(
+					response => {
+						this.productCategories = response.categories;
+					},
+					error => {
+						console.log('********** ViewAllProductCategoriesComponent.ts: ' + error);
+					}
+				)
+			},
+			error => {
+				console.log('********** DeleteServiceComponent.ts: ' + error);
+			}
+		);
+		this.displayDelete = false;
 	}
 
 }
